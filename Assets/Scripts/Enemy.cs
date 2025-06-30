6b1e722e7239d5de153a1051d19c2f2f8d8994eb
Ekrem Bas <ekrembas03@outlook.com>
2025-06-30 16:08:44 +0300

    enemy oyuncu takibi ai ile yapildi

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,12 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
 
     [SerializeField] private GameObject target; // oyuncu
+    private NavMeshAgent agent; // NavMeshAgent bileşeni
     float speed = 2f; // düşmanın hareket hızı
+
+    void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.speed = speed; // düşmanın hızını ayarla
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,10 +25,6 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // oyuncunun pozisyonu
-        Vector3 targetPos = new Vector3(target.transform.position.x, transform.position.y, target.transform.position.z);
-        transform.LookAt(targetPos); // oyuncuya bak
-        // oyuncuya doğru hareket et
-        transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+        agent.SetDestination(target.transform.position); // düşmanı oyuncuya doğru hareket ettir
     }
 }
